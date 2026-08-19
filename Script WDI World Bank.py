9b4040e2abcc90f5ea06fd829cce23494f879aec
Aldalis Ferreira França Gomes <aldalis.gomes@gmail.com>
2026-08-19 16:22:53 -0300

    language update

--- Script WDI World Bank.py
+++ Script WDI World Bank.py
@@ -8,98 +8,98 @@ import matplotlib.pyplot as plt
 
 #%% Loading the datasets
 # We use an argument to handle NAs in the WDI data
-dados_wdi = pd.read_excel('WDI World Bank.xlsx', na_values="..")
-dados_grupo = pd.read_excel('WDI Income Group.xlsx')
-dados_paises = pd.read_excel('WDI Country.xlsx')
+wdi_data = pd.read_excel('WDI World Bank.xlsx', na_values="..")
+group_data = pd.read_excel('WDI Income Group.xlsx')
+country_data = pd.read_excel('WDI Country.xlsx')
 
 #%% Basic data information
-dados_wdi.info()
+wdi_data.info()
 
 #%% Unique elements of the variables
-dados_wdi['Country Name'].unique()
-dados_wdi['Series Name'].unique()
-dados_wdi['Topic'].unique()
+wdi_data['Country Name'].unique()
+wdi_data['Series Name'].unique()
+wdi_data['Topic'].unique()
 
 #%% Renaming columns for easier access
-dados_wdi.rename(columns={'Country Name':'pais',
-                          'Country Code':'cod_pais',
-                          'Series Name':'serie',
-                          'Series Code':'cod_serie',
-                          '2021 [YR2021]':'ano_2021',
-                          'Topic':'topico'}, inplace=True)
+wdi_data.rename(columns={'Country Name':'country',
+                          'Country Code':'country_code',
+                          'Series Name':'series',
+                          'Series Code':'series_code',
+                          '2021 [YR2021]':'year_2021',
+                          'Topic':'topic'}, inplace=True)
 
 #%% Analyzing the last rows
-dados_wdi.tail(n=20) # The last rows are references and will not be used
+wdi_data.tail(n=20) # The last rows are references and will not be used
 
 #%% Excluding the final rows (we add 1 position in the sequence)
-dados_wdi= dados_wdi.iloc[0:383572,]
+wdi_data= wdi_data.iloc[0:383572,]
 
 # Reviewing the data
-dados_wdi['pais'].tail(n=20)
+wdi_data['country'].tail(n=20)
 
 #%% Selecting health topics (They start with 'Health')
-dados_saude = dados_wdi[dados_wdi['topico'].str.startswith('Health')]
+health_data = wdi_data[wdi_data['topic'].str.startswith('Health')]
 
 #%% Let's change the structure of health_data
 # Variables: series /// Observations: countries
-dados_saude = pd.pivot(dados_saude,
-                       index=['pais','cod_pais'],
-                       columns=['serie'],
-                       values='ano_2021')
+health_data = pd.pivot(health_data,
+                       index=['country','country_code'],
+                       columns=['series'],
+                       values='year_2021')
 # Returning to the numeric index
-dados_saude.reset_index(inplace=True)
+health_data.reset_index(inplace=True)
 
 #%% Cleaning observations
 # In country_data 'Country' shows the country code
 # let's change the variable name to make merges using this key
-dados_paises.rename(columns={'Country':'cod_pais'},
+country_data.rename(columns={'Country':'country_code'},
                     inplace=True)
 # Merge: the variable goes to the end of the dataset
-dados_saude= pd.merge(dados_saude, dados_paises,
+health_data= pd.merge(health_data, country_data,
                       how='left',
-                      on='cod_pais')
+                      on='country_code')
 # The 'nan' values are not countries, let's exclude them with a filter 
-dados_saude=dados_saude[~dados_saude['Name'].isna()].reset_index(drop=True)
+health_data=health_data[~health_data['Name'].isna()].reset_index(drop=True)
 # Let's remove the column that will no longer be used
-dados_saude.drop(columns=['Name'], inplace=True)
+health_data.drop(columns=['Name'], inplace=True)
 #%% Variable cleaning
 # Many variables only present NAs
 # axis=1 -> refers to the columns
 # how='all' -> drop if all elements are nan
-dados_saude.dropna(axis=1,how='all',inplace=True)
+health_data.dropna(axis=1,how='all',inplace=True)
 #%% Add the "income group" category to the final dataset
 # Add a grouping variable that is in 'group_data'
 # Selecting the variables of interest
-dados_grupo_select=dados_grupo[['Code','Income Group']].copy()
+group_data_select=group_data[['Code','Income Group']].copy()
 # Changing the key name for the link between DataFrames
-dados_grupo_select.rename(columns={'Code':'cod_pais'},inplace=True)
+group_data_select.rename(columns={'Code':'country_code'},inplace=True)
 # Performing the merge
-dados_saude=pd.merge(dados_saude,dados_grupo_select,how='left',on='cod_pais')
+health_data=pd.merge(health_data,group_data_select,how='left',on='country_code')
 
 #%% Let's reorganize the column position 
 # Removes the 'Income Group' column from health_data, then stores this column in "organize" [.pop()],
-organizar=dados_saude.pop('Income Group')
+organize=health_data.pop('Income Group')
 # Put the 'Income Group' column,
 # with the new name 'Group',
 # in position 2 of the dataset health_data
-dados_saude.insert(2,'Group',organizar)
+health_data.insert(2,'Group',organize)
 
 #%% For example, if it is a study about diabetes
 # Let's see in which position the variable of interest is:
-col_pos=dados_saude.columns 
+col_pos=health_data.columns 
 print(col_pos) ## It is in position 23
 # Descriptive statistics of column 23
-dados_saude.iloc[:,23].describe()
+health_data.iloc[:,23].describe()
 
 # Statistics by groups
-estat_grupo=dados_saude.iloc[:,[2,23]].groupby('Group').mean().reset_index()
+group_stats=health_data.iloc[:,[2,23]].groupby('Group').mean().reset_index()
 
 
 # %% Chart
 plt.figure(figsize=(15,9), dpi = 600)
-ax = sns.barplot(data=estat_grupo, x=estat_grupo.iloc[:,0], y=estat_grupo.iloc[:,1])
+ax = sns.barplot(data=group_stats, x=group_stats.iloc[:,0], y=group_stats.iloc[:,1])
 for container in ax.containers: ax.bar_label(container, fmt='%.2f', padding=3, fontsize=12)
-plt.xlabel('Grupo',fontsize=15)
+plt.xlabel('Group',fontsize=15)
 plt.ylabel('Diabetes prevalence (% of population ages 20 to 79)', fontsize=15)
 plt.xticks(fontsize=14)
 plt.yticks(fontsize=14)
